a11cc0433318859f6804bd963e5eeefc6bc4f498
Rodolfo González <rodolfo.gonzalez@globant.com>
2019-10-14 17:09:13 -0500

    add senders that have data from rcp request

--- contents/generate.rb
+++ contents/generate.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'json'
 
+mcoErrorCodes = [1, 3, 4]
 
 if ENV['RD_CONFIG_MCO_COMMAND_LINE'] == nil
    $stderr.puts "no RD_CONFIG_MCO_COMMAND_LINE environment provided, check mco_command_line plugin configuration property"
@@ -17,7 +18,7 @@ end
 
 cmdLineResult=$?
 
-if cmdLineResult != 0
+if mcoErrorCodes.include?(cmdLineResult)
    $stderr.puts "mco command line  #{cmdLine} failed (result code:  #{cmdLineResult})"
    exit 1
 end
@@ -40,24 +41,29 @@ print "<project>\n"
 
 $i = 0;
 while $i < inv.length do
-
-   invEntry=inv[$i];
+   invEntry = inv[$i];
    sender = invEntry["sender"]
-   architecture = invEntry["data"]["facts"]["architecture"]
-   osfamily = invEntry["data"]["facts"]["osfamily"]
-   kernel = invEntry["data"]["facts"]["kernel"]
-   kernelrelease = invEntry["data"]["facts"]["kernelrelease"]
-   tags=invEntry["data"]["classes"].join(",")
-   username=invEntry["data"]["facts"]["id"]
-   operatingsystemrelease=invEntry["data"]["facts"]["operatingsystemrelease"]
+   
+   if invEntry["data"]["facts"].kind_of?(Hash) && invEntry["data"]["facts"].has_key?("ipaddress_eth0")
+      
+      tags = (invEntry["data"]["classes"].kind_of?(Array) && invEntry["data"]["classes"].any? ? invEntry["data"]["classes"].join(",") : '')
+      hostname = ( invEntry["data"]["facts"].has_key?("ipaddress_eth0")? invEntry["data"]["facts"]["ipaddress_eth0"] : '')
+      architecture = ( invEntry["data"]["facts"].has_key?("architecture")? invEntry["data"]["facts"]["architecture"] : '')
+      osfamily = ( invEntry["data"]["facts"].has_key?("osfamily")? invEntry["data"]["facts"]["osfamily"] : '')
+      kernel = ( invEntry["data"]["facts"].has_key?("kernel")? invEntry["data"]["facts"]["kernel"] : '')
+      kernelrelease = ( invEntry["data"]["facts"].has_key?("kernelrelease")? invEntry["data"]["facts"]["kernelrelease"] : '')
+      username = ( invEntry["data"]["facts"].has_key?("id")? invEntry["data"]["facts"]["id"] : '')
+      operatingsystemrelease = ( invEntry["data"]["facts"].has_key?("operatingsystemrelease")? invEntry["data"]["facts"]["operatingsystemrelease"] : '')
 
-   if username == ""
-      username="root"
+      if username == ""
+         username="root"
+      end
+   
+      print "   <node name=\"#{sender}\" description=\"#{osfamily} #{operatingsystemrelease}\" tags=\"#{tags}\" hostname=\"#{hostname}\"  osArch=\"#{architecture}\" osFamily=\"#{osfamily}\" osName=\"#{kernel}\" osVersion=\"#{kernelrelease}\" username=\"#{username}\"/>\n"
    end
 
-   print "   <node name=\"#{sender}\" description=\"#{osfamily} #{operatingsystemrelease}\" tags=\"#{tags}\" hostname=\"#{sender}\"  osArch=\"#{architecture}\" osFamily=\"#{osfamily}\" osName=\"#{kernel}\" osVersion=\"#{kernelrelease}\" username=\"#{username}\"/>\n"
-
    $i += 1;
+
 end
 
 print "</project>\n"
